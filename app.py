ffb028e214e815c020753176e8f0de129644275b
zhuoyifei43-commits <zhuoyifei43@gmail.com>
2025-11-21 06:32:02 +0000

    1

--- app.py
+++ app.py
@@ -7,5 +7,11 @@ def index():
     return(render_template("index.html"))
 
 
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    q = request.form.get("g")  
+    print(q)  
+    return (render_template("main.html"))
+
 if __name__ == "__main__":
     app.run()
